a913b7dda9059fc452426aa21cce6f7c31e51522
Jennifer Evans <jenevans3ten@gmail.com>
2020-12-19 16:26:29 -0500

    starter console app with xUnit test project & gitignore file

---
+++ word-frequency/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace word_frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
