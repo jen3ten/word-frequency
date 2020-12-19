6532e1d66ac664e2e91c7af4fe787cd39b520952
Jennifer Evans <jenevans3ten@gmail.com>
2020-12-19 17:15:05 -0500

    test streamreader connection to stopword text file

--- word-frequency/Program.cs
+++ word-frequency/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace word_frequency
 {
@@ -6,7 +7,34 @@ namespace word_frequency
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                using (StreamReader reader = new StreamReader("C:/Users/jenev/source/repos/CleFedReserveBank/word-frequency/word-frequency/Data/stopwords.txt"))
+                {
+                    bool eof = false;
+                    string line;
+                    int count = 0;
+                    while (!eof)
+                    {
+                        line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            eof = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine(line);
+                            count++;
+                        }
+                    }
+                    Console.WriteLine($"line count is {count}");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR: file could not be read");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
