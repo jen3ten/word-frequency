91f7e4cf360d98677a494d2050f77dc0282a52ea
Jennifer Evans <jenevans3ten@gmail.com>
2020-12-20 20:42:17 -0500

    new TrimListString method failed test

---
+++ word-frequency.Tests/DataCleanerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace word_frequency.Tests
+{
+    public class DataCleanerTests
+    {
+        public DataCleaner sut;
+        public DataCleanerTests()
+        {
+            sut = new DataCleaner();
+        }
+
+        [Fact]
+        public void TrimListStrings_Should_Remove_Leading_And_Trailing_Spaces()
+        {
+            List<string> testList = new List<string> { "  one", " two ", "three  ", "four" };
+            List<string> expectedList = new List<string> { "one", "two", "three", "four" };
+
+            Assert.Equal(expectedList, sut.TrimListStrings(testList));
+        }
+
+    }
+}
---
+++ word-frequency/DataCleaner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace word_frequency
+{
+    public class DataCleaner
+    {
+
+        public List<string> TrimListStrings(List<string> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
