a73593815ce275e6d7f86e47fd7426f5bea23d71
Jennifer Evans <jenevans3ten@gmail.com>
2020-12-22 15:59:05 -0500

    split data into array of strings

--- word-frequency/Program.cs
+++ word-frequency/Program.cs
@@ -21,30 +21,40 @@ namespace word_frequency
             DataReader reader = new DataReader(filePath);
             DataCleaner cleaner = new DataCleaner();
 
-            if (reader.DefineStream(stopWordsDataFile))
-            {
-                stopWords = reader.ConvertTextFileToList(reader.Stream);
+            //if (reader.DefineStream(stopWordsDataFile))
+            //{
+            //    stopWords = reader.ConvertTextFileToList(reader.Stream);
 
-                for (int i = 0; i < stopWords.Count; i++)
-                {
-                    stopWords[i] = cleaner.TrimString(stopWords[i]);
-                    stopWords[i] = cleaner.StringToLowerCase(stopWords[i]);
-                    Console.WriteLine(stopWords[i]);
-                }
-            }
-            Console.WriteLine();
+            //    for (int i = 0; i < stopWords.Count; i++)
+            //    {
+            //        stopWords[i] = cleaner.TrimString(stopWords[i]);
+            //        stopWords[i] = cleaner.StringToLowerCase(stopWords[i]);
+            //        Console.WriteLine(stopWords[i]);
+            //    }
+            //}
+            //Console.WriteLine();
 
             if (reader.DefineStream(text1DataFile))
             {
                 text1Data = reader.ConvertTextFileToString(reader.Stream);
-                Console.WriteLine(text1Data);
+                char[] delimiters = { ' ', '-' };
+                string[] text1Words = text1Data.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < text1Words.Length; i++)
+                {
+                    Console.WriteLine(text1Words[i]);
+                }
             }
             Console.WriteLine();
 
             if (reader.DefineStream(text2DataFile))
             {
                 text2Data = reader.ConvertTextFileToString(reader.Stream);
-                Console.WriteLine(text2Data);
+                char[] delimiters = { ' ', '-'};
+                string[] text2Words = text2Data.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < text2Words.Length; i++)
+                {
+                    Console.WriteLine(text2Words[i]);
+                }
             }
             Console.WriteLine();
 
