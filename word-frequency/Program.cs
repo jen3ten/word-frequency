0a417836d0ce7883fc5a5b85de5bba4f8e476f2a
Jennifer Evans <jenevans3ten@gmail.com>
2020-12-22 20:15:06 -0500

    refactored Program class to simplify creation of stop word array

--- word-frequency/Program.cs
+++ word-frequency/Program.cs
@@ -14,7 +14,8 @@ namespace word_frequency
             string text1DataFile = "Text1.txt";
             string text2DataFile = "Text2.txt";
 
-            List<string> stopWords;
+            //List<string> stopWords;
+            string stopWordsData;
             string text1Data;
             string text2Data;
 
@@ -22,40 +23,51 @@ namespace word_frequency
 
             if (reader.DefineStream(stopWordsDataFile))
             {
-                stopWords = reader.ConvertTextFileToList(reader.Stream);
+                // Iteration 1: Convert text file to list, then trim and convert to lower case
+                //stopWords = reader.ConvertTextFileToList(reader.Stream);
 
-                for (int i = 0; i < stopWords.Count; i++)
+                //for (int i = 0; i < stopWords.Count; i++)
+                //{
+                //    stopWords[i] = DataCleaner.TrimString(stopWords[i]);
+                //    stopWords[i] = DataCleaner.StringToLowerCase(stopWords[i]);
+                //    Console.WriteLine(stopWords[i]);
+                //}
+
+                // Iteration 2: Convert text file to string, then split at Delimiters
+                    // whitespace and tabs are automatically removed
+                    // conversion to lower case is not necessary
+                stopWordsData = reader.ConvertTextFileToString(reader.Stream);
+                char[] delimiters = { ' ', '\t' };
+                string[] stopWords = DataCleaner.SplitStringAtDelimiters(stopWordsData, delimiters);
+                for (int i = 0; i < stopWords.Length; i++)
                 {
-                    stopWords[i] = DataCleaner.TrimString(stopWords[i]);
-                    stopWords[i] = DataCleaner.StringToLowerCase(stopWords[i]);
                     Console.WriteLine(stopWords[i]);
                 }
             }
             Console.WriteLine();
 
-            if (reader.DefineStream(text1DataFile))
-            {
-                text1Data = reader.ConvertTextFileToString(reader.Stream);
-                char[] delimiters = { ' ', '-' };
-                string[] text1Words = DataCleaner.SplitStringAtDelimiters(text1Data, delimiters);
-                for (int i = 0; i < text1Words.Length; i++)
-                {
-                    Console.WriteLine(text1Words[i]);
-                }
-            }
-            Console.WriteLine();
+            //if (reader.DefineStream(text1DataFile))
+            //{
+            //    text1Data = reader.ConvertTextFileToString(reader.Stream);
+            //    char[] delimiters = { ' ', '-' };
+            //    string[] text1Words = DataCleaner.SplitStringAtDelimiters(text1Data, delimiters);
+            //    for (int i = 0; i < text1Words.Length; i++)
+            //    {
+            //        Console.WriteLine(text1Words[i]);
+            //    }
+            //}
+            //Console.WriteLine();
 
-            if (reader.DefineStream(text2DataFile))
-            {
-                text2Data = reader.ConvertTextFileToString(reader.Stream);
-                char[] delimiters = { ' ', '-'};
-                string[] text2Words = DataCleaner.SplitStringAtDelimiters(text2Data, delimiters);
-                for (int i = 0; i < text2Words.Length; i++)
-                {
-                    Console.WriteLine(text2Words[i]);
-                }
-            }
-            Console.WriteLine();
+            //if (reader.DefineStream(text2DataFile))
+            //{
+            //    text2Data = reader.ConvertTextFileToString(reader.Stream);
+            //    char[] delimiters = { ' ', '-'};
+            //    string[] text2Words = DataCleaner.SplitStringAtDelimiters(text2Data, delimiters);
+            //    for (int i = 0; i < text2Words.Length; i++)
+            //    {
+            //        Console.WriteLine(text2Words[i]);
+            //    }
+            //}
 
             Console.ReadKey();
         }
